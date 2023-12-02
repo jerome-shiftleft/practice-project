6debe8f5d46a31c39d2bdb4ef6e3ae57db960c78
jerome-shiftleft <jerome@theshiftleft.com>
2023-12-03 07:04:55 +0800

    progress

---
+++ src/components/ProjectsSidebar.jsx
@@ -0,0 +1,14 @@
+function ProjectsSidebar() {
+  return(
+    <>
+      <aside>
+        <h2>Your Projects</h2>
+        <div>
+          <button>+ Add Button</button>
+        </div>
+      </aside>
+    </>
+  )
+}
+
+export default ProjectsSidebar;
