2ce0567c5fdbc2c87bc79cd7f0869fd1032efdae
jerome-shiftleft <jerome@theshiftleft.com>
2023-12-03 16:51:51 +0800

    progress

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,13 +3,11 @@ import TextField from "@mui/material/TextField";
 function Input({ label, textarea, ...props }) {
   return (
     <>
-      <p>        
-        {textarea ? (
-          <TextField multiline size="small" label={label} {...props} />
-        ) : (
-          <TextField size="small" label={label} {...props} />
-        )}
-      </p>
+      {textarea ? (
+        <TextField multiline size="small" label={label} {...props} />
+      ) : (
+        <TextField size="small" label={label} {...props} />
+      )}
     </>
   );
 }
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,18 +1,19 @@
 import Input from "./Input";
+import Button from '@mui/material/Button';
 
 function NewProject() {
   return (
     <>
-      <div>        
-        <div>
+      <div id="content">
+        <div className="input-fields">
           <Input label="Title"  />
           <Input label="Description" textarea />
           <Input label="Due Date"  />
         </div>
-        <menu>
-          <li><button>Cancel</button></li>
-          <li><button>Save</button></li>
-        </menu>
+        <div className="action-buttons">
+          <Button variant="contained" color="secondary">Cancel</Button>
+          <Button variant="contained">Save</Button>
+        </div>
       </div>
     </>
   )
