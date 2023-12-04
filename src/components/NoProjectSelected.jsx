cdcd8e72296bfa1d2a87aabd061e208ebb18934d
jerome-shiftleft <jerome@theshiftleft.com>
2023-12-04 20:29:40 +0800

    tested emotion css

--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -1,23 +1,24 @@
 import { Box } from "@mui/system";
+import { css } from '@emotion/css'
 import Button from "@mui/material/Button";
 import noProjectImage from "../assets/no-projects.png";
 
 function NoProjectSelected() {
   return (
     <>
-      <Box sx={{
-        '& .no-project-icon': {
-          display: 'block',
-          width: '120px'
+      <div className={'content '+ css`
+        .no-project-icon {
+          display: block;
+          width: 120px;
         }
-      }} id="no-project" className="content">
+      `} id="no-project">
         <img className="no-project-icon" src={noProjectImage} alt="An empty task list" />
         <h2>No Project Selected</h2>
         <p>Select a project or get started with a new one</p>
         <p>
           <Button variant="contained">Create a new project</Button>
         </p>
-      </Box>
+      </div>
     </>
   );
 }
