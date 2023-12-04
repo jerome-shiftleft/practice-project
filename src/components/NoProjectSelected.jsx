5ddd7ccc97788472947b296510f93d97e8e0c9e1
jerome-shiftleft <jerome@theshiftleft.com>
2023-12-04 20:35:48 +0800

    progress

--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -1,17 +1,10 @@
-import { Box } from "@mui/system";
-import { css } from '@emotion/css'
 import Button from "@mui/material/Button";
 import noProjectImage from "../assets/no-projects.png";
 
 function NoProjectSelected() {
   return (
     <>
-      <div className={'content '+ css`
-        .no-project-icon {
-          display: block;
-          width: 120px;
-        }
-      `} id="no-project">
+      <div id="no-project" className="content">
         <img className="no-project-icon" src={noProjectImage} alt="An empty task list" />
         <h2>No Project Selected</h2>
         <p>Select a project or get started with a new one</p>
